db3f60a6c5b9670c32667ff843cc6fe443effd23
jjesswan <jessica_wan@brown.edu>
2024-01-23 21:42:14 -0500

    update helpful links

--- src/components/sections/Links.tsx
+++ src/components/sections/Links.tsx
@@ -42,18 +42,34 @@ export default function Links() {
               <Text variant="capitalBody" color="lightGrey" mb=".5rem">
                 WEBSITES
               </Text>
-              <Link to="/">
+              <Link to="https://canvas.brown.edu/courses/1094692" target="blank">
                 <Button variant="solidLinkSmall" mb="1rem">
                   CANVAS
                 </Button>
               </Link>
-              <Link to="/">
-                <Button variant="solidLinkSmall">EDSTEM</Button>
+              <Link
+                to="https://www.gradescope.com/courses/715064"
+                target="blank"
+              >
+                <Button variant="solidLinkSmall" mb="1rem">
+                  GRADESCOPE
+                </Button>
               </Link>
-            </Flex>
+              <Link to="https://talie.town/cs1300_spring24/">
+                <Button variant="solidLinkSmall" mb="1rem">
+                  EDSTEM
+                </Button>
+              </Link>
+              <Link
+                to="https://docs.google.com/document/d/1qvwOuIJ6BKtt3l-noSUTa553aQ1beHsMG6lk-qgg1Aw/edit?usp=sharing"
+                target="blank"
+              >
+                <Button variant="solidLinkSmall">Syllabus</Button>
+              </Link>
+              {/* </Flex> */}
 
-            {/* COURSE DOCS */}
-            <Flex flexDir={"column"}>
+              {/* COURSE DOCS */}
+              {/* <Flex flexDir={"column"}>
               <Text variant="capitalBody" color="lightGrey" mb=".5rem">
                 Course Docs
               </Text>
@@ -70,38 +86,26 @@ export default function Links() {
               <Link to="/">
                 <Button variant="solidLinkSmall">Deployment Guide</Button>
               </Link>
+            </Flex> */}
             </Flex>
-          </Flex>
 
-          <Flex
+            {/* <Flex
             flexDir={{ md: "row", base: "column" }}
             alignItems={"flex-start"}
             gap={"2rem"}
-          >
+          > */}
             {/* COURSE FORMS */}
             <Flex flexDir={"column"}>
               <Text variant="capitalBody" color="lightGrey" mb=".5rem">
                 ASSIGNMENTS
               </Text>
-              <Link to="/">
-                <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
-                  <TiDownloadOutline />
-                  Assignment Handin
-                </Button>
-              </Link>
-              <Link to="/">
+              <Link to="https://talie.town/cs1300_spring24/">
                 <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
                   <TiDownloadOutline />
                   Studio Handin
                 </Button>
               </Link>
-              <Link to="/">
-                <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
-                  <TiDownloadOutline />
-                  Late Pass Form
-                </Button>
-              </Link>
-              <Link to="/">
+              <Link to="https://forms.gle/emqG5dWFDehqRfmZ6" target="blank">
                 <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
                   <TiDownloadOutline />
                   Regrade Request Form
@@ -114,22 +118,28 @@ export default function Links() {
               <Text variant="capitalBody" color="lightGrey" mb=".5rem">
                 logistical
               </Text>
-              <Link to="/">
+              <Link to="https://forms.gle/frfoVeTNFLZ3vHdV7" target="blank">
                 <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
                   <TiDownloadOutline />
                   Studio Swap Form
                 </Button>
               </Link>
-              <Link to="/">
+              <Link to="https://talie.town/cs1300_spring24/">
                 <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
                   <TiDownloadOutline />
                   Feedback Form
                 </Button>
               </Link>
-              <Link to="/">
+              <Link to="https://forms.gle/qkrmKFtPEqZKSQy56" target="blank">
                 <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
                   <TiDownloadOutline />
-                  Dean's/Doctor's Note
+                  Dean's/Doctor's Note Form
+                </Button>
+              </Link>
+              <Link to="https://forms.gle/Te6pAGBNiF5aZa3g6" target="blank">
+                <Button variant="solidLinkSmall" gap=".2rem" mb=".5rem">
+                  <TiDownloadOutline />
+                  Accomodations Form
                 </Button>
               </Link>
             </Flex>
